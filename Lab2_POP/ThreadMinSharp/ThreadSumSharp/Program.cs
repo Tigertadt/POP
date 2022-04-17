203e5939cefae7b36f7f14b93bc57641deb45262
Tigertadt <58162052+Tigertadt@users.noreply.github.com>
2022-04-17 14:55:20 +0300

    Update Program.cs

--- Lab2_POP/ThreadMinSharp/ThreadSumSharp/Program.cs
+++ Lab2_POP/ThreadMinSharp/ThreadSumSharp/Program.cs
@@ -1,13 +1,15 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 
 namespace ThreadSumSharp
 {
     class Program
     {
-        private static readonly int dim = 100;
-        private static readonly int threadNum = 2;
-        public int min = 100000;
+        public static int dim = 10;
+        
+        public static int threadNum=4;       
+        public int min = 10000;
         public int index = 0;
 
 
@@ -16,7 +18,7 @@ namespace ThreadSumSharp
         static void Main(string[] args)
         {
             Program main = new Program();
-            main.InitArr();
+            main.InitArr(dim);            
            
             Console.WriteLine("\nНайменше число масиву: {0}, його індекс: {1}", main.ParallelMin(), main.index_res);
             Console.ReadLine();
@@ -27,11 +29,47 @@ namespace ThreadSumSharp
 
         private long ParallelMin()
         {
-            thread[0] = new Thread(StarterThread);
-            thread[0].Start(new Bound(0, dim / 2));
-            thread[1] = new Thread(StarterThread);
-            thread[1].Start(new Bound(dim / 2, dim));
-           
+            //int[] arr1 = new int[arr.Length/threadNum];
+
+            //for (int i = 0; i<threadNum;i++)
+            //{
+            //    for(int j = 0;j<arr1.Length;j++)
+            //    {
+
+            //        arr1[j] = new int[]
+            //        {                        
+            //            arr[j * threadNum],
+            //            arr[j * threadNum+1],
+            //            arr[j * threadNum+2],
+            //            arr[j * threadNum+3],
+            //        };
+            //        thread[i] = new Thread(StarterThread);
+            //        thread[i].Start(new Bound(arr1[i], arr1[i + 1]));
+            //    }
+
+            //}
+
+            int n = arr.Length / threadNum;
+            int[] arr1 = new int[n];
+            for(int j = 0; j<n;j++)
+            {
+                arr1[j] = arr[j];
+
+                for (int i = 0; i < threadNum; i++)
+                {
+                    thread[i] = new Thread(StarterThread);
+                    thread[i].Start(new Bound(arr1[0], arr1[arr1.Length-1]));
+                }
+            }
+            
+
+            //thread[0] = new Thread(StarterThread);
+            //thread[0].Start(new Bound(0, arr.Length / 2));
+            //thread[1] = new Thread(StarterThread);
+            //thread[1].Start(new Bound(arr.Length / 2, arr.Length));
+
+
+
 
             lock (lockerForCount)
             {
@@ -44,24 +82,29 @@ namespace ThreadSumSharp
             return min;
         }
 
-        private readonly int[] arr = new int[dim];
+        public readonly int[] arr = new int[dim];
 
-        private void InitArr() //инициализация массива
+        private int InitArr(int dim) //инициализация массива
         {
+            int length = arr.Length;
             Random rand = new Random();
             for (int i = 0; i < arr.Length; i++)
             {
-                arr[i] = rand.Next(-1000, 1000);
+                arr[i] = rand.Next(0, 1000);
+                arr[3] = -9;
                 Console.Write("{0} ", arr[i] );
             }
+            return length;
         }
 
         class Bound
         {
             public Bound(int startIndex, int finishIndex) // метод определения размера массива
             {
+
                 StartIndex = startIndex;
                 FinishIndex = finishIndex;
+            
             }
 
             public int StartIndex { get; set; }
@@ -103,7 +146,7 @@ namespace ThreadSumSharp
             if (this.min<min_res)
             {
                 min_res = this.min;
-                index_res = this.index;
+                index_res = this.index+1;
             
             }
         }
